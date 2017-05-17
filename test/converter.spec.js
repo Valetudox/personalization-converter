13b6e30c072dede85027b37d181f5cff1db30a16
esysuser <esysuser@probanapos02-mba.local>
2017-05-17 09:06:11 +0200

    Fix typos and add test cases for converter.spec

--- test/converter.spec.js
+++ test/converter.spec.js
@@ -1,4 +1,4 @@
-let expect    = require("chai").expect;
+let expect = require("chai").expect;
 let converter = require("../app/converter");
 let cheerio = require('cheerio');
 
@@ -24,7 +24,7 @@ describe("#convertPersonalizationObjects", function() {
        expect(converter.convertPersonalizationObjects(testObject).length).to.equal(1);
     });
 
-    it("should return an object where the id's should be the same as the input object's id", function() {
+    it("should return a list of objects where the id's should be the same as the input object's id", function() {
         let testObject = {
             id: 54321,
             content: `<span class="cbNonEditable" e-personalization="1">First Name</span>
@@ -37,26 +37,47 @@ describe("#convertPersonalizationObjects", function() {
         expect(isActualIdsMatchExpected).to.be.true;
     });
 
-    it("should return an object where the contextField should be the same as the e-personalization value", function() {
+    it("should return a list of objects where the contextField should be the same as the e-personalization value", function() {
         let value = 3;
         let testObject = {
             id: 54321,
             content: `<span class="cbNonEditable" e-personalization="${value}">First Name</span>`
         };
-        expect(converter.convertPersonalizationObjects(testObject)[0].contextField).to.equal(value);
+        expect(converter.convertPersonalizationObjects(testObject).pop().contextField).to.equal(value);
     });
 
-    it("should return an object where the text property should match the span's text value", function() {
-        let text = `test text`
+    it("should return a list of objects where the text property should match the span's text value", function() {
+        let text = `test text`;
         let testObject = {
             id: 54321,
             content: `<span class="cbNonEditable" e-personalization="1">${text}</span>`
         };
-        expect(converter.convertPersonalizationObjects(testObject)[0].text).to.equal(text);
+        expect(converter.convertPersonalizationObjects(testObject).pop().text).to.equal(text);
+    });
+
+    it.skip("should return a list of objects where the personalization in the content property should be replaced", function() {
+        let testObject = {
+            id: 54321,
+            content:             
+            `<div>
+                <span class="cbNonEditable" e-personalization="1">text</span>
+            </div>`
+        };
+        expectedContent = 
+            `<div>
+                #TOKEN
+            </div>`;
+        expect(converter.convertPersonalizationObjects(testObject).pop().content.replace(/ /g, ''))
+            .to.equal(expectedContent.replace(/ /g, ''));
     });
 });
 
 describe("#convertPersonalizationList", function() {
+
+    it("should convert an empty list", function () {
+         expect(converter.convertPersonalizationList([]).length).to.equal(0);
+     });
+
     it("should convert a list of 3 items", function () {
          let testObjects = [{
              id: 11111,
@@ -70,7 +91,6 @@ describe("#convertPersonalizationList", function() {
              id: 33333,
              content: `<span class="cbNonEditable" e-personalization="3">Last Name</span>`
          }];
-        
-         expect(testObjects.length).to.equal(converter.convertPersonalizationList(testObjects).length);
+         expect(converter.convertPersonalizationList(testObjects).length).to.equal(3);
      });
 });
